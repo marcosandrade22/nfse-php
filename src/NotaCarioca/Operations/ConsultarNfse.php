f2bedf4bc963f7dbb56d0247aaa39bffc83ae553
marcosandrade22 <suportetuxinfo@gmail.com>
2020-09-13 15:18:40 -0300

    Update ConsultarNfse.php

--- src/NotaCarioca/Operations/ConsultarNfse.php
+++ src/NotaCarioca/Operations/ConsultarNfse.php
@@ -33,12 +33,16 @@ class ConsultarNfse extends NotaCariocaOperationBase
 
         $responseArr = [];
         if (isset($resultArr['ListaNfse']) and isset($resultArr['ListaNfse']['CompNfse'])) {
+              $count_result =  count($resultArr['ListaNfse']['CompNfse']);
+           if($count_result <=1){
+               foreach ($resultArr['ListaNfse']['CompNfse'] as $nfse) {
+                  $responseArr[] = $nfse['InfNfse'];
+                  }
+            }else{
             foreach ($resultArr['ListaNfse']['CompNfse'] as $nfse) {
-                $responseArr[] = $nfse['Nfse']['InfNfse'];
+               $responseArr[] = $nfse['Nfse']['InfNfse'];
             }
-        }
-
-        return $responseArr;
+            }
     }
 
     /**
